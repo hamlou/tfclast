463013788cf21cc063e32b7fb5752d2543004a36
Auto Push <autopush@example.com>
2026-08-15 12:28:53 +0200

    Updates

---
+++ assets/index-BOulnrn2.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-zx2IkAn5.js","assets/index-B17Lil2f.js","assets/index-DGZHhp5G.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-B17Lil2f.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-zx2IkAn5.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
